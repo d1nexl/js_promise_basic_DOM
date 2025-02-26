028e304a589cd0c4cbac04488da2743b982a9347
Vladyslav Kutsyn <vladislavkucn@gmail.com>
2025-02-26 13:36:13 +0100

    add soltuion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
+/* eslint-disable */
+const logo = document.querySelector('.logo');
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+promise1.then(() => {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.textContent = 'Обіцянку вирішено!';
+  document.body.appendChild(div);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject();
+  }, 3000);
+});
+
+promise2.catch(() => {
+  const div = document.createElement('div');
+  div.classList.add('message', 'error-message');
+  div.textContent = 'Обіцянку  не вирішено!';
+  document.body.appendChild(div);
+});
